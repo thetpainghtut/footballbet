f824ba902acae1d7a082f6d93d3af756bdc33de4
thetpainghtut <thetpainghtut.bf@gmail.com>
2021-01-15 15:33:01 +0630

    ui changes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -163,7 +163,7 @@
   <!-- /.container -->
 
   <!-- Footer -->
-  <footer class="py-5 bg-primary fixed-bottom">
+  <footer class="py-5 bg-primary">
     <div class="container">
       <p class="m-0 text-center text-white">Copyright &copy; Your Website 2020</p>
     </div>
